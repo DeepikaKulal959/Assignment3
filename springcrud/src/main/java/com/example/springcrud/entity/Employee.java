8d3e5ded7e82f7aac03158bd79acd4fd8826e3f2
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-10-12 10:40:18 +0530

    updated Employee.java

--- springcrud/src/main/java/com/example/springcrud/entity/Employee.java
+++ springcrud/src/main/java/com/example/springcrud/entity/Employee.java
@@ -7,30 +7,45 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee {
 	@Id
 	@GeneratedValue
 	private int id;
 	private String name;
 	private int age;
-	
+	private String status;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 }
