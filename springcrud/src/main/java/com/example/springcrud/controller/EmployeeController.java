7cd78af3d2dd4a561fae5cc0c8358677d69522d4
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-10-12 10:32:47 +0530

    updated EmployeeController.java

--- springcrud/src/main/java/com/example/springcrud/controller/EmployeeController.java
+++ springcrud/src/main/java/com/example/springcrud/controller/EmployeeController.java
@@ -16,37 +16,28 @@ import com.example.springcrud.service.EmployeeService;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-	
+
 	@Autowired
-	private EmployeeService  employeeService;
-	
+	private EmployeeService employeeService;
+
 	@PostMapping
-    public Employee addEmployee(@RequestBody Employee employee) {
-    	return employeeService.save(employee);
-    }
-	
-	@GetMapping(path="/{id}")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		return employeeService.save(employee);
+	}
+
+	@GetMapping(path = "/{id}")
 	public Employee getEmployee(@PathVariable int id) {
 		return employeeService.getEmployee(id);
 	}
-	
+
 	@PutMapping
 	public Employee updateEmployee(@RequestBody Employee employee) {
 		return employeeService.update(employee);
 	}
-	
-	@DeleteMapping(path="/{id}")
-	public String deleteEmployee(@PathVariable int id) {
-		return employeeService.delete(id);
-	}
-	
-	public EmployeeService getEmployeeService() {
-		return employeeService;
-	}
 
-	
-	public void setEmployeeService(EmployeeService employeeService) {
-		this.employeeService = employeeService;
+	@DeleteMapping
+	public Employee deleteEmployee(@RequestBody Employee employee) {
+		return employeeService.delete(employee.getId());
 	}
 
 }
